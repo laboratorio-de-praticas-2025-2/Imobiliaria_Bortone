7d78ae5cfbdd4310f5d352b0fd4f86795f37f37e
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-21 16:20:38 -0300

    fix(boas-vindas): adiciona links aos botões

--- front-end/src/app/bem-vindo/page.js
+++ front-end/src/app/bem-vindo/page.js
@@ -10,8 +10,8 @@ export default function BoasVindasPage() {
         <Flex vertical align='center' gap="large" className='boas-vindas-content'>
             <h1 className='login-title text-2xl text-[var(--primary)]'>Bem Vindo!</h1>
             <Flex vertical className='login-form-container' gap="large" align='center'>
-                <Button type="primary" htmlType="submit" className='login-button w-[100%]'>Criar uma conta</Button>
-                <Button type="primary" htmlType="submit" className='login-sec-button w-[100%]'>Login</Button>
+                <Button type="primary" htmlType="submit" className='login-button w-[100%]' href='/cadastro'>Criar uma conta</Button>
+                <Button type="primary" htmlType="submit" className='login-sec-button w-[100%]' href='/login'>Login</Button>
                 <Flex gap={14}>
                     {socialMedias.map((media) => (
                         <SocialMediaIcon key={media.alt} src={media.src} alt={media.alt} />
