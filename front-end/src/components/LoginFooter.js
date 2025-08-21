bc9d45b8cf7d03308d19050e5bfa5ddb2d5f9990
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-21 16:18:00 -0300

    refactor(login): cria constante para socialMedias

--- front-end/src/components/LoginFooter.js
+++ front-end/src/components/LoginFooter.js
@@ -1,24 +1,6 @@
 import { Flex } from "antd";
 import SocialMediaIcon from "./SocialMediaIcon";
-
-const socialMedias = [
-    {
-        src: "/images/icons/google.svg",
-        alt: "Google"
-    },
-    {
-        src: "/images/icons/facebook.svg",
-        alt: "Facebook"
-    },
-    {
-        src: "/images/icons/instagram.svg",
-        alt: "Instagram"
-    },
-    {
-        src: "/images/icons/linkedin.svg",
-        alt: "LinkedIn"
-    }
-]
+import { socialMedias } from "@/constants/socialMedias";
 
 export default function LoginFooter() {
   return (
