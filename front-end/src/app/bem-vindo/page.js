16d7e95f7a4880f34d8bb80c1ee5793bbea26598
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-21 16:31:24 -0300

    feat(boas-vindas): implementa tela de splash

--- front-end/src/app/bem-vindo/page.js
+++ front-end/src/app/bem-vindo/page.js
@@ -1,24 +1,69 @@
-"use client"
-import { Button, Flex } from 'antd';
-import SocialMediaIcon from '@/components/SocialMediaIcon';
-import { socialMedias } from '@/constants/socialMedias';
+"use client";
+import { Button, Flex } from "antd";
+import SocialMediaIcon from "@/components/SocialMediaIcon";
+import { socialMedias } from "@/constants/socialMedias";
+import SplashScreen from "@/components/SplashScreen";
+import { useEffect, useState } from "react";
 
 export default function BoasVindasPage() {
+  const [showSplash, setShowSplash] = useState(true);
+  const [animateOut, setAnimateOut] = useState(false);
+
+  useEffect(() => {
+    if (showSplash) {
+      const timer1 = setTimeout(() => setAnimateOut(true), 2000);
+      const timer2 = setTimeout(() => setShowSplash(false), 2500);
+      return () => {
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+      };
+    }
+  }, [showSplash]);
+
+  if (showSplash) {
+    return <SplashScreen animateOut={animateOut} />;
+  }
+
   return (
     <div>
-        <div className="image-header"/>
-        <Flex vertical align='center' gap="large" className='boas-vindas-content'>
-            <h1 className='login-title text-2xl text-[var(--primary)]'>Bem Vindo!</h1>
-            <Flex vertical className='login-form-container' gap="large" align='center'>
-                <Button type="primary" htmlType="submit" className='login-button w-[100%]' href='/cadastro'>Criar uma conta</Button>
-                <Button type="primary" htmlType="submit" className='login-sec-button w-[100%]' href='/login'>Login</Button>
-                <Flex gap={14}>
-                    {socialMedias.map((media) => (
-                        <SocialMediaIcon key={media.alt} src={media.src} alt={media.alt} />
-                    ))}
-                </Flex>
-            </Flex>
+      <div className="image-header" />
+      <Flex vertical align="center" gap="large" className="boas-vindas-content">
+        <h1 className="login-title text-2xl text-[var(--primary)]">
+          Bem Vindo!
+        </h1>
+        <Flex
+          vertical
+          className="login-form-container"
+          gap="large"
+          align="center"
+        >
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-button w-[100%]"
+            href="/cadastro"
+          >
+            Criar uma conta
+          </Button>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-sec-button w-[100%]"
+            href="/login"
+          >
+            Login
+          </Button>
+          <Flex gap={14}>
+            {socialMedias.map((media) => (
+              <SocialMediaIcon
+                key={media.alt}
+                src={media.src}
+                alt={media.alt}
+              />
+            ))}
+          </Flex>
         </Flex>
+      </Flex>
     </div>
   );
 }
