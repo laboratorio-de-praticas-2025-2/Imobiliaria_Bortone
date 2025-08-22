309080de66ef0e10002dc20316c072c55e433681
Daniel Augusto Mandira <105872910+DanielMandira@users.noreply.github.com>
2025-08-22 18:55:24 -0300

    Fix: Corrige erros de merge  (#31)
    * feat(devops): implementa botsde verificação (#2)
    
    * Feature/frontend configuracao ambiente (#8)
    
    * feat: adiciona fonte padrão
    
    * chore: remove estilização para modo escuro
    
    * feat: atualiza metadata
    
    * feat: atualiza o favicon
    
    * feat: adiciona paleta de cores
    
    * docs: Adicionando documentação do banco de dados (#18)
    
    * docs: Adicionando documentação do banco de dados
    
    * docs: Corrigindo descrição e Linkando ao Repositório Correto
    
    * Tela de login e cadastro #12 (#27)
    
    * feat: adiciona imagem do header
    
    * deps: adiciona Ant Design
    
    * feat(login): adiciona pagina de login mobile
    
    * feat(login): adiciona pagina para desktop
    
    * fix(login): adiciona hover nos botões
    
    * refactor(login): componentiza Footer e icones
    
    * fix: muda as extensões de jsx para js
    
    * feat(cadastro): adiciona página de cadastro
    
    * fix: remove import redundante
    
    * fix(login): muda nome do formulário
    
    * refactor(login): cria constante para socialMedias
    
    * feat(boas-vindas): adiciona tela de boas vindas
    
    * fix(boas-vindas): adiciona links aos botões
    
    * feat(boas-vindas): implementa tela de splash
    
    * fix: adiciona favicon no public e global style em styles
    
    ---------
    
    Co-authored-by: Amanda Oliveira <100137341+amandazzoc@users.noreply.github.com>
    Co-authored-by: Diego Baltazar de Souza <99719728+SouzaDiegoCl@users.noreply.github.com>

---
+++ front-end/src/app/login/page.js
@@ -0,0 +1,44 @@
+"use client"
+import LoginFooter from '@/components/LoginFooter';
+import Link from 'next/link';
+import { Form, Input, Button, Flex } from 'antd';
+
+export default function LoginPage() {
+  return (
+    <div>
+        <div className="image-header"/>
+        <Flex vertical align='center' gap="large" className='login-content'>
+            <h1 className='login-title text-2xl text-[var(--primary)]'>Faça seu login</h1>
+            <Flex vertical className='login-form-container'>
+                <Form name='login' autoComplete='off'>
+                    <Flex vertical align='center'>
+                        <Form.Item 
+                            name="email"
+                            rules={[{ required: true, message: 'Por favor, insira seu email!' }]}
+                            className='login-form-item'
+                        >
+                            <Input placeholder='Digite seu email:'/>
+                        </Form.Item>
+                        <Form.Item 
+                            name="password"
+                            rules={[{ required: true, message: 'Por favor, insira sua senha!' }]}
+                            className='login-form-item'
+                        >
+                            <Input.Password placeholder='Digite sua senha:'/>
+                        </Form.Item>
+                        <Form.Item>
+                            <Flex vertical align='center' gap="small">
+                                <Button type="primary" htmlType="submit" className='login-button'>Entrar</Button>
+                                <Flex>
+                                    <Link href="/cadastro" className='redirect-link'>Cadastre-se</Link>
+                                </Flex>
+                            </Flex>
+                        </Form.Item>
+                    </Flex>
+                </Form>
+                <LoginFooter />
+            </Flex>
+        </Flex>
+    </div>
+  );
+}
