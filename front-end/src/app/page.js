31f9ebba0487b5dae00660d48ae467db8b9e15e1
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-23 17:43:39 -0300

    feat(home): implementa cards de CTA

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { Divider } from "antd";
+import CTACard from "@/components/home/CTACard";
 import Header from "@/components/home/Header";
 
 export default function Home() {
@@ -7,6 +8,10 @@ export default function Home() {
     <>
       <Header />
       <Divider size="large"/>
+      <div className="ctacards flex gap-10 px-16 pt-7 md:flex-row flex-col">
+        <CTACard title="Faça sua proposta" description="Gostou de um imóvel? Faça uma proposta de valor e siga para as próximas etapas!" buttonText="Fazer Proposta" className="md:w-[75%]"/>
+        <CTACard title="Fale com alguém" description="Não sabe como prosseguir? Seja atendido via chat!" buttonText="Abrir chat" className="md:w-[25%]"/>
+      </div>
     </>
   );
 }
