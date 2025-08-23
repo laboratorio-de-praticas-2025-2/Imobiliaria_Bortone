e09e5cd790f3f6be06d269add982294c2cad8458
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-23 17:45:53 -0300

    fix(home): ajusta estilo do card

--- front-end/src/components/home/CTACard.js
+++ front-end/src/components/home/CTACard.js
@@ -2,7 +2,7 @@ import { Flex, Button } from "antd"
 
 export default function CTACard({ className, title, description, buttonText }) {
     return (
-        <div className={`p-7 bg-[#DEE1F0] flex align-middle ${className}`}>
+        <div className={`p-7 bg-[#DEE1F0] flex align-middle rounded-xl ${className}`}>
             <Flex vertical gap="large" justify="center">
                 <p className="text-3xl font-extrabold text-[var(--primary)]">{title}</p>
                 <p className="text-lg text-[var(--primary)]">{description}</p>
