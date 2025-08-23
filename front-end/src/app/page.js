a9726f418d7c2de8ea5774921c2961d60a95e721
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-22 21:15:18 -0300

    feat(home): adiciona textos do header

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -1,9 +1,40 @@
+"use client";
 import HomeNavbar from "@/components/HomeNavbar";
+import { Flex, Input, Button } from "antd";
+
+const { Search } = Input;
+
+const onSearch = (value) => console.log(value);
 
 export default function Home() {
   return (
     <div className="header-home">
       <HomeNavbar />
+      <div className="ps-16 pt-7">
+        <Flex vertical style={{ width: "50%" }} gap="60px">
+          <Search
+            placeholder="Pesquisa"
+            onSearch={onSearch}
+            className="home-search-bar"
+          />
+          <Flex vertical gap="large">
+            <span className="text-[var(--primary)] text-6xl font-extrabold uppercase">
+              Compre, alugue ou anuncie
+            </span>
+            <span className="text-[var(--primary)] text-xl font-extrabold uppercase">
+              Confira Imóveis à partir de
+              <p>R$ 250.000,00</p>
+            </span>
+            <Button
+              variant="outlined"
+              shape="round"
+              className="btn-marcar-visita"
+            >
+              MARQUE UMA VISITA
+            </Button>
+          </Flex>
+        </Flex>
+      </div>
     </div>
   );
 }
