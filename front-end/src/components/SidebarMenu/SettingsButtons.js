2ec779eaf82cd9ff7f7e04061dced6cacde0abf8
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-24 21:42:05 -0300

    fix: ajusta cor do texto dos botoes

--- front-end/src/components/SidebarMenu/SettingsButtons.js
+++ front-end/src/components/SidebarMenu/SettingsButtons.js
@@ -7,7 +7,8 @@ export default function SettingsButtons({ onUndo, onApply }) {
       {/* Botão Desfazer */}
       <button
         onClick={onUndo}
-        className=" w-full rounded-lg border-3 border-[#374A8C54] text-gray-400 font-semibold bg-transparent hover:bg-[#1b2235] transition"
+        className=" w-full rounded-lg border-3 border-[#374A8C54] font-semibold bg-transparent hover:bg-[#1b2235] transition"
+        style={{ color: "#767A8B " }}
       >
         Desfazer
       </button>
@@ -15,23 +16,26 @@ export default function SettingsButtons({ onUndo, onApply }) {
       {/* Botão Aplicar */}
       <button
         onClick={onApply}
-        className="w-full py-2 rounded-lg bg-[var(--secondary)] text-white font-bold  hover:bg-[#d88500] transition"
-      > 
-      <Flex align='center' justify='center' >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={3}
-          stroke="white"
-          className="w-8 h-8"
-          
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
-        </svg>
-        Aplicar
-      </Flex>
-
+        className="w-full py-2 rounded-lg bg-[var(--secondary)] font-bold  hover:bg-[#d88500] transition"
+        style={{ color: "white" }}
+      >
+        <Flex align="center" justify="center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={3}
+            stroke="white"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M5 13l4 4L19 7"
+            />
+          </svg>
+          Aplicar
+        </Flex>
       </button>
     </div>
   );
