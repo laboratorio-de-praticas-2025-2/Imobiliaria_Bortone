a89043b510202dd3eb3d4d7988e739b719c0cf40
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-25 13:20:53 -0300

    feat: adiciona splash para aguardar renderização

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -5,10 +5,30 @@ import Header from "@/components/home/Header";
 import PropriedadesSelecionadas from "@/components/home/PropriedadesSelecionadas";
 import PropriedadesPerto from "@/components/home/PropriedadesPerto";
 import HomeFooter from "@/components/home/HomeFooter";
+import SplashScreen from "@/components/SplashScreen";
+import { useEffect, useState } from "react";
 
 const isLoggedIn = false;
 
 export default function Home() {
+  const [showSplash, setShowSplash] = useState(true);
+  const [animateOut, setAnimateOut] = useState(false);
+
+  useEffect(() => {
+    if (showSplash) {
+      const timer1 = setTimeout(() => setAnimateOut(true), 2000);
+      const timer2 = setTimeout(() => setShowSplash(false), 1500);
+      return () => {
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+      };
+    }
+  }, [showSplash]);
+
+  if (showSplash) {
+    return <SplashScreen animateOut={animateOut} />;
+  }
+
   return (
     <>
       <Header />
