d0002f29f872949051aefa749efd07a3dd1e0e74
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-26 17:51:50 -0300

    feat: adiciona divisor

--- front-end/src/app/imoveis/page.js
+++ front-end/src/app/imoveis/page.js
@@ -1,11 +1,13 @@
 import HomeNavbar from "@/components/home/HomeNavbar";
 import Filtros from "@/components/vitrine/Filtros";
+import { Divider } from "antd";
 
 export default function ImoveisPage() {
     return (
       <>
         <HomeNavbar />
         <Filtros/>
+        <Divider size="large" />
       </>
     );
 }
