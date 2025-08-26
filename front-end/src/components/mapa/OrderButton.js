f39f72f75704bcc177e221b2c4b1803a4b36944c
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-25 22:22:26 -0300

    fix: ajusta funcionalidade de botão

--- front-end/src/components/mapa/OrderButton.js
+++ front-end/src/components/mapa/OrderButton.js
@@ -1,29 +1,42 @@
 "use client";
+import { useState } from "react";
 import { Button } from "antd";
 import { HiArrowsUpDown } from "react-icons/hi2";
 
 export default function OrderButton({ onToggle }) {
+    const [rotated, setRotated] = useState(false);
 
     const handleClick = () => {
-        if (onToggle) onToggle(); 
-    }
+        setRotated((prev) => !prev);
+        if (onToggle) onToggle();
+    };
 
-  return (
-    <Button
-      onClick={handleClick}
-      style={{
-        width: "50px",
-        height: "50px",
-        background: "var(--background)",
-        border: "var(--fog-gray) 2px solid",
-        borderRadius: "1000px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: "15px",
-      }}
-      className="!shadow-lg"
-      icon={<HiArrowsUpDown size={20} color="374a8c"/>}
-    />
-  );
+    return (
+        <Button
+            onClick={handleClick}
+            style={{
+                width: "50px",
+                height: "50px",
+                background: "var(--background)",
+                border: "var(--fog-gray) 2px solid",
+                borderRadius: "1000px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                padding: "15px",
+            }}
+            className="!shadow-lg"
+            icon={
+                <span
+                    style={{
+                        display: "inline-block",
+                        transition: "transform 0.3s",
+                        transform: rotated ? "rotate(180deg)" : "rotate(0deg)",
+                    }}
+                >
+                    <HiArrowsUpDown size={20} color="374a8c" />
+                </span>
+            }
+        />
+    );
 }
