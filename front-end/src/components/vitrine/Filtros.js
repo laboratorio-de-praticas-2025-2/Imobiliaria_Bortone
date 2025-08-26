5c33b7e61cb57914c98a7ae93d0b6c6d3df47b8e
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-26 17:51:40 -0300

    feat: implementa filtros

--- front-end/src/components/vitrine/Filtros.js
+++ front-end/src/components/vitrine/Filtros.js
@@ -1,7 +1,24 @@
-import LocationInput from "./LocationInput";
+import LocationInput from "./LocationInput"
+import DropdownFilter from "./DropdownFilter"
+import PesquisaAvancada from "./PesquisaAvancada";
+import { Flex, Space } from "antd"
+
+const optionsBuy = ["Comprar", "Alugar"];
+const optionsRooms = ["1", "2", "3", "+4"];
+const optionsBathrooms = ["1", "2", "3", "+4"];
 
 export default function Filtros() {
     return (
-        <LocationInput />
-    )
+        <div className="py-7 px-18">
+            <Flex justify="space-between" style={{width: '100%'}}>
+                <Space size="large">
+                <LocationInput />
+                <DropdownFilter options={optionsBuy} />
+                <DropdownFilter options={optionsRooms} placeholder={"Quartos"}/>
+                <DropdownFilter options={optionsBathrooms} placeholder={"Banheiros"}/>
+                </Space>
+                <PesquisaAvancada/>
+            </Flex>
+        </div>
+    );
 }
