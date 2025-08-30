3ef54c440af3da9482c426893856958cdae9ed9b
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-30 01:07:55 -0300

    feat: implementa tabela

--- front-end/src/app/imoveis/InnerImoveisPage.js
+++ front-end/src/app/imoveis/InnerImoveisPage.js
@@ -3,6 +3,7 @@ import HomeNavbar from "@/components/home/HomeNavbar";
 import Filtros from "@/components/vitrine/Filtros";
 import { useFilterData } from "@/context/FilterDataContext";
 import { Divider } from "antd";
+import TabelaImoveis from "@/components/vitrine/TabelaImoveis";
 
 export default function InnerImoveisPage({ qtdImoveis }) {
   const {
@@ -14,13 +15,16 @@ export default function InnerImoveisPage({ qtdImoveis }) {
       <HomeNavbar />
       <Filtros />
       <Divider size="large" style={{ margin: 0 }} />
-      <div className="py-11 md:px-17 px-0">
+      <div className="py-11 px-3 md:px-17">
         <p className="text-xl text-[var(--primary)] font-bold">
           {qtdImoveis} imóveis disponíveis
         </p>
         <p className="text-xl text-[var(--primary)] font-bold">
           para venda em {regiao}
         </p>
+      </div>
+      <div className="pb-11 md:px-17 sm:px-3 px-0 flex justify-center">
+        <TabelaImoveis />
       </div>
     </>
   );
