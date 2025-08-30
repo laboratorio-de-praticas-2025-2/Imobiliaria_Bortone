45f6d997c2dfd5b08d09ad91bf0b8a2b858c95c0
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-30 17:42:32 -0300

    feat: implementa accordion

--- front-end/src/components/faq/FaqContent.js
+++ front-end/src/components/faq/FaqContent.js
@@ -1,7 +1,12 @@
-export default function FaqContent() {
-    return (
-        <div className="md:px-24 md:py-24 px-12 py-9">
-            <p className="md:text-3xl text-xl font-black md:ps-5 md:text-shadow-none text-shadow-lg">Perguntas Frequentes</p>
-        </div>
-    )
+import QuestionAccordion from "./QuestionAccordion";
+
+export default function FaqContent({ faqData }) {
+  return (
+    <div className="md:px-24 md:py-24 px-6 py-9 flex flex-col gap-10">
+      <p className="md:text-3xl text-xl font-black md:ps-5 ps-7 md:text-shadow-none text-shadow-lg">
+        Perguntas Frequentes
+      </p>
+      <QuestionAccordion faqData={faqData}/>
+    </div>
+  );
 }
