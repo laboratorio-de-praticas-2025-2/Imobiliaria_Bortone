5a01da08e4d07feebce1e3509342c36dd098840c
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-30 01:06:39 -0300

    feat: adiciona carrossel de imagem

---
+++ front-end/src/components/vitrine/ImageCarroussel.js
@@ -0,0 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+export default function ImageCarroussel({ imovel }) {
+  return (
+    <div className="w-full">
+      {/* Box fixo p/ o carrossel */}
+      <div className="relative w-full aspect-video overflow-hidden rounded-xl">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          navigation
+          pagination={{ clickable: true }}
+          className="w-full h-full image-carroussel-imoveis"
+        >
+          {imovel.imagens.map((img, index) => (
+            <SwiperSlide key={index} className="!w-full !h-full flex">
+              <img
+                src={img.url_imagem}
+                alt={img.descricao}
+                className="w-full h-full object-cover"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+}
