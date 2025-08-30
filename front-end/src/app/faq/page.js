ccca650e908036c3d38363be9fb21b190ddcc90a
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-30 17:42:46 -0300

    feat: adiciona esqueleto de chamada da api

--- front-end/src/app/faq/page.js
+++ front-end/src/app/faq/page.js
@@ -1,23 +1,41 @@
-import { Divider } from "antd";
-import HomeNavbar from "@/components/home/HomeNavbar";
-import HomeFooter from "@/components/home/HomeFooter";
+'use client'
 import FaqContent from "@/components/faq/FaqContent";
+import HomeFooter from "@/components/home/HomeFooter";
+import HomeNavbar from "@/components/home/HomeNavbar";
+import { MOCKFAQ } from "@/constants/faq";
+import { Divider } from "antd";
+import { useEffect, useState } from "react";
 
 export default function FaqPage() {
-    return (
-      <>
-        <HomeNavbar className="md:!bg-[#050d2de3]" />
-        <div className="banner-faq flex flex-col md:gap-4 justify-center md:ps-52 ps-10">
-          <p className="md:text-7xl text-2xl font-bold text-white">
-            Perguntas<p>frequentes</p>
-          </p>
-          <p className="md:text-4xl text-sm text-white">
-            Ficou com alguma dúvida? <p>Nós temos as repostas!</p>
-          </p>
-        </div>
-        <FaqContent />
-        <Divider size="large" />
-        <HomeFooter />
-      </>
-    );
+  const [faqData, setFaqData] = useState([]);
+
+  const fetchFaq = async () => {
+    
+    // Simulando uma chamada de API com dados mockados
+    setFaqData(MOCKFAQ);
+  };
+
+  useEffect(() => {
+    fetchFaq();
+  }, []);
+
+  return (
+    <>
+      <HomeNavbar className="md:!bg-[#050d2de3]" />
+      <div className="banner-faq flex flex-col md:gap-4 justify-center md:ps-52 ps-10">
+        <p className="md:text-7xl text-2xl font-bold text-white">
+          Perguntas
+          <br />
+          frequentes
+        </p>
+        <p className="md:text-4xl text-sm text-white">
+          Ficou com alguma dúvida? <br />
+          Nós temos as repostas!
+        </p>
+      </div>
+      <FaqContent faqData={faqData} />
+      <Divider size="large" />
+      <HomeFooter />
+    </>
+  );
 }
