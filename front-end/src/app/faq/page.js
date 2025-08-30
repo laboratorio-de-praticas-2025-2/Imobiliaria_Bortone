f4e31b6d0a7db3085a5e57c02f46a7c213f61a7f
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-30 02:55:05 -0300

    feat: adiciona banner

--- front-end/src/app/faq/page.js
+++ front-end/src/app/faq/page.js
@@ -6,6 +6,14 @@ export default function FaqPage() {
     return (
       <>
         <HomeNavbar className="md:!bg-[#050d2de3]" />
+        <div className="banner-faq flex flex-col md:gap-4 justify-center md:ps-52 ps-10">
+          <p className="md:text-7xl text-2xl font-bold text-white">
+            Perguntas<p>frequentes</p>
+          </p>
+          <p className="md:text-4xl text-sm text-white">
+            Ficou com alguma dúvida? <p>Nós temos as repostas!</p>
+          </p>
+        </div>
         <Divider size="large" />
         <HomeFooter />
       </>
