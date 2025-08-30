261aa1bdf52bb072d84852b6b065a49bcae3d7fe
Amanda Oliveira <amandaol7b@gmail.com>
2025-08-30 18:05:50 -0300

    fix: ajusta navbar

--- front-end/src/constants/navLinks.js
+++ front-end/src/constants/navLinks.js
@@ -1,14 +1,14 @@
 
 import { RiHome2Line } from "react-icons/ri";
 import { FaSignHanging } from "react-icons/fa6";
-import { HiMiniMegaphone } from "react-icons/hi2";
+import { HiMapPin } from "react-icons/hi2";
 import { HiMiniNewspaper } from "react-icons/hi2";
 import { AiFillQuestionCircle } from "react-icons/ai";
 
 export const navLinks = [
   { name: "Home", path: "/", icon: RiHome2Line },
   { name: "Imóveis", path: "/imoveis", icon: FaSignHanging },
-  { name: "Anuncie", path: "/anuncie", icon: HiMiniMegaphone },
+  { name: "Mapa", path: "/mapa", icon: HiMapPin },
   { name: "Blog", path: "/blog", icon: HiMiniNewspaper },
   { name: "FAQ", path: "/faq", icon: AiFillQuestionCircle },
 ];
