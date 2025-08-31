c7f25847decc9cbcd73f272b31031c37fbf1b5ec
Vinicius Leal Alves <avieirakunzen@gmail.com>
2025-08-31 14:51:38 -0300

    update sequelize-config.js

--- back-end/src/config/sequelize-config.js
+++ back-end/src/config/sequelize-config.js
@@ -1,12 +1,14 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-const connection = new Sequelize({
-  DB_DIALECT: process.env.DB_DIALECT,
-  DB_HOST: process.env.DB_HOST,
-  DB_USER: process.env.DB_USER,
-  DB_PASS: process.env.DB_PASS,
-  DB_DATABASE: process.env.DB_DATABASE,
-});
+const connection = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 export default connection;
