4a94a4ef2a4ce3db51b136129ab97fdcd153d45a
BrunoDavies3 <brunodavies321@gmail.com>
2025-08-31 23:47:41 -0300

    feat(services): criado services.js

--- back-end/src/services/service.js
+++ back-end/src/services/service.js
@@ -1,1 +1,27 @@
-// Esse é apenas um arquivo para manter o diretório ativo
+const { Publicidade } = require("../models/model");
+
+class PublicidadeService {
+  async updatePublicidade(id, novosDados) {
+    try {
+      const publicidade = await Publicidade.findByPk(id);
+
+      if (!publicidade) {
+        throw new Error(`Publicidade com ID ${id} não encontrada.`);
+      }
+
+      publicidade.titulo = novosDados.titulo || publicidade.titulo;
+      publicidade.conteudo = novosDados.conteudo || publicidade.conteudo;
+      publicidade.url_imagem = novosDados.url_imagem || publicidade.url_imagem;
+      publicidade.usuario_id = novosDados.usuario_id || publicidade.usuario_id;
+      publicidade.ativo = novosDados.ativo !== undefined ? novosDados.ativo : publicidade.ativo;
+
+      await publicidade.save();
+
+      return publicidade;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+module.exports = new PublicidadeService();
