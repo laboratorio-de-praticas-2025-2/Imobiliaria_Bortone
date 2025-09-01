032585d7ecdef62be8e0ebdb471a7647d3e224e2
MauricioRVeiga <mauricio.rgv15@gmail.com>
2025-08-31 23:55:59 -0300

    feat (model) criado publicidade.js

---
+++ back-end/src/models/publicidade.js
@@ -0,0 +1,39 @@
+export default (sequelize, DataTypes) => {
+  const Publicidade = sequelize.define('Publicidade', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    titulo: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    conteudo: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    url_imagem: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    usuario_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'usuario',
+        key: 'id'
+      }
+    }
+  }, {
+    tableName: 'publicidade',
+    timestamps: false
+  });
+
+  // Relacionamentos
+  Publicidade.associate = (models) => {
+    Publicidade.belongsTo(models.Usuario, { foreignKey: 'usuario_id' });
+  };
+
+  return Publicidade;
+};
