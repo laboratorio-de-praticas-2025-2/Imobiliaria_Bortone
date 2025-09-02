35cc3c5d66641034bb4ed86603af124acaa4a14d
Amanda Oliveira <amandaol7b@gmail.com>
2025-09-02 20:44:11 -0300

    feat: adiciona componentes para fazer o CMS

---
+++ front-end/src/components/cms/Table.js
@@ -0,0 +1,7 @@
+export default function Table({data, columns, children}) {
+    return(
+        <div className="w-full">
+            {children}
+        </div>
+    )
+}
---
+++ front-end/src/components/cms/index.js
@@ -0,0 +1,13 @@
+import Body from "./Body";
+import Table from "./Table";
+import TableHeader from "./TableHeader";
+
+export { Body, Table, TableHeader };
+
+const CMS = {
+  Body,
+  Table,
+  TableHeader,
+};
+
+export default CMS;
