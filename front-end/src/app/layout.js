52f6ef22f0baff47680867793f41b51e903c3e31
Amanda Oliveira <amandaol7b@gmail.com>
2025-09-02 20:43:41 -0300

    feat: adiciona estilo

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -6,6 +6,7 @@ import "@/styles/simulacao.css"
 import "@/styles/imoveis.css";
 import "@/styles/blog.css";
 import "@/styles/faq.css";
+import "@/styles/cms.css";
 import 'antd/dist/reset.css';
 import Script from "next/script";
 
