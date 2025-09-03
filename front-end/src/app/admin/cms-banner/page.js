0993889e0dda02511526b5eaa0cd8cc7210cc02b
Amanda Oliveira <amandaol7b@gmail.com>
2025-09-02 21:13:54 -0300

    feat: implementa banner

--- front-end/src/app/admin/cms-banner/page.js
+++ front-end/src/app/admin/cms-banner/page.js
@@ -1,8 +1,18 @@
 'use client';
+import { useEffect, useState } from "react";
 import CMS from "@/components/cms";
 import { FaImage } from "react-icons/fa6";
+import Card from "@/components/cms/Card";
+import { bannersMock } from "@/constants/banner";
 
-export default function cmsBannerPage() {
+export default function CmsBannerPage() {
+    const [banners, setBanners] = useState([]);
+
+    useEffect(() => {
+        // Fetch banners from API or database
+        setBanners(bannersMock);
+    }, []);
+
     const onSearch = (value) => {
         console.log("Search:", value);
     };
@@ -22,6 +32,17 @@ export default function cmsBannerPage() {
       <CMS.Body title={"Banners"}>
         <CMS.Table>
           <CMS.TableHeader buttonText={'Novo Banner'} buttonIcon={<FaImage />} onSearch={onSearch} onClickNew={onClickNew} handleSelectOrder={handleSelectOrder} filterData={filterData} updateFilterData={updateFilterData} />
+          <CMS.TableBody>
+            {banners.length > 0 ? (
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                {banners.map((banner) => (
+                  <Card key={banner.id} banner={banner} />
+                ))}
+              </div>
+            ) : (
+              <p>No banners found.</p>
+            )}
+          </CMS.TableBody>
         </CMS.Table>
       </CMS.Body>
     );
