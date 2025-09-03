1d02f2f72e6e1330d72added5850301e0d5132a3
Amanda Oliveira <amandaol7b@gmail.com>
2025-09-02 21:13:35 -0300

    feat: adiciona componente do corpo da tabela

---
+++ front-end/src/components/cms/TableBody.js
@@ -0,0 +1,3 @@
+export default function TableBody({children}) {
+  return <div className="bg-[#E5E5E5] shadow-md p-7 w-full">{children}</div>;
+}
--- front-end/src/components/cms/index.js
+++ front-end/src/components/cms/index.js
@@ -1,13 +1,15 @@
 import Body from "./Body";
 import Table from "./Table";
 import TableHeader from "./TableHeader";
+import TableBody from "./TableBody";
 
-export { Body, Table, TableHeader };
+export { Body, Table, TableHeader, TableBody };
 
 const CMS = {
   Body,
   Table,
   TableHeader,
+  TableBody,
 };
 
 export default CMS;
