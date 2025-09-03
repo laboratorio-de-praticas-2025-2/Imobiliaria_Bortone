aca9521f94a4f2faf39c07eb403e8704c6a34545
Amanda Oliveira <amandaol7b@gmail.com>
2025-09-02 21:42:05 -0300

    feat: adiciona footer

--- front-end/src/app/admin/cms-banner/page.js
+++ front-end/src/app/admin/cms-banner/page.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { useEffect, useState } from "react";
 import CMS from "@/components/cms";
 import { FaImage } from "react-icons/fa6";
@@ -6,44 +6,57 @@ import Card from "@/components/cms/Card";
 import { bannersMock } from "@/constants/banner";
 
 export default function CmsBannerPage() {
-    const [banners, setBanners] = useState([]);
+  const [banners, setBanners] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 8;
 
-    useEffect(() => {
-        // Fetch banners from API or database
-        setBanners(bannersMock);
-    }, []);
+  useEffect(() => {
+    setBanners(bannersMock);
+  }, []);
 
-    const onSearch = (value) => {
-        console.log("Search:", value);
-    };
-    const onClickNew = () => {
-        console.log("New Banner");
-    }
-    const handleSelectOrder = (value) => {
-        console.log("Selected order:", value);
-    }
-    const filterData = {};
+  // fatia os banners conforme página
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const paginatedBanners = banners.slice(startIndex, endIndex);
 
-    const updateFilterData = (newData) => {
-        console.log("Update filter data:", newData);
-    }
+  const onSearch = (value) => console.log("Search:", value);
+  const onClickNew = () => console.log("New Banner");
+  const handleSelectOrder = (value) => console.log("Selected order:", value);
+  const filterData = {};
+  const updateFilterData = (newData) =>
+    console.log("Update filter data:", newData);
 
-    return (
-      <CMS.Body title={"Banners"}>
-        <CMS.Table>
-          <CMS.TableHeader buttonText={'Novo Banner'} buttonIcon={<FaImage />} onSearch={onSearch} onClickNew={onClickNew} handleSelectOrder={handleSelectOrder} filterData={filterData} updateFilterData={updateFilterData} />
-          <CMS.TableBody>
-            {banners.length > 0 ? (
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                {banners.map((banner) => (
-                  <Card key={banner.id} banner={banner} />
-                ))}
-              </div>
-            ) : (
-              <p>No banners found.</p>
-            )}
-          </CMS.TableBody>
-        </CMS.Table>
-      </CMS.Body>
-    );
+  return (
+    <CMS.Body title={"Banners"}>
+      <CMS.Table>
+        <CMS.TableHeader
+          buttonText="Novo Banner"
+          buttonIcon={<FaImage />}
+          onSearch={onSearch}
+          onClickNew={onClickNew}
+          handleSelectOrder={handleSelectOrder}
+          filterData={filterData}
+          updateFilterData={updateFilterData}
+        />
+        <CMS.TableBody>
+          {paginatedBanners.length > 0 ? (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              {paginatedBanners.map((banner) => (
+                <Card key={banner.id} banner={banner} />
+              ))}
+            </div>
+          ) : (
+            <p>No banners found.</p>
+          )}
+        </CMS.TableBody>
+
+        {/* Paginador controlado */}
+        <CMS.TableFooter
+          postsData={banners}
+          pageSize={pageSize}
+          onPageChange={setCurrentPage}
+        />
+      </CMS.Table>
+    </CMS.Body>
+  );
 }
--- front-end/src/components/cms/index.js
+++ front-end/src/components/cms/index.js
@@ -2,14 +2,16 @@ import Body from "./Body";
 import Table from "./Table";
 import TableHeader from "./TableHeader";
 import TableBody from "./TableBody";
+import TableFooter from "./TableFooter";
 
-export { Body, Table, TableHeader, TableBody };
+export { Body, Table, TableHeader, TableBody, TableFooter };
 
 const CMS = {
   Body,
   Table,
   TableHeader,
   TableBody,
+  TableFooter
 };
 
 export default CMS;
