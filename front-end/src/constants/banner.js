375cdb752751e340dd39236fdb22aeb6257aee0f
Amanda Oliveira <amandaol7b@gmail.com>
2025-09-02 21:13:45 -0300

    feat: adiciona mock de banner

---
+++ front-end/src/constants/banner.js
@@ -0,0 +1,75 @@
+export const bannersMock = [
+    {
+        id: 1,
+        url_imagem: "/images/imovel1.jpg",
+        descricao: "Banner 1",
+        ativo: true
+    },
+    {
+        id: 2,
+        url_imagem: "/images/imovel2.jpeg",
+        descricao: "Banner 2",
+        ativo: false
+    },
+    {
+        id: 3,
+        url_imagem: "/images/post1.jpeg",
+        descricao: "Banner 3",
+        ativo: true
+    },
+    {
+        id: 4,
+        url_imagem: "/images/post2.jpg",
+        descricao: "Banner 4",
+        ativo: false
+    },
+    {
+        id: 5,
+        url_imagem: "/images/post3.jpg",
+        descricao: "Banner 5",
+        ativo: true
+    },
+    {
+        id: 6,
+        url_imagem: "/images/post4.jpg",
+        descricao: "Banner 6",
+        ativo: false
+    },
+    {
+        id: 7,
+        url_imagem: "/images/post5.jpeg",
+        descricao: "Banner 7",
+        ativo: true
+    },
+    {
+        id: 8,
+        url_imagem: "/images/post6.jpeg",
+        descricao: "Banner 8",
+        ativo: false
+    },
+    {
+        id: 9,
+        url_imagem: "/images/post7.jpeg",
+        descricao: "Banner 9",
+        ativo: true
+    },
+    {
+        id: 10,
+        url_imagem: "/images/slide1.png",
+        descricao: "Banner 10",
+        ativo: false
+    },
+    {
+        id: 11,
+        url_imagem: "/images/slide2.png",
+        descricao: "Banner 11",
+        ativo: true
+    },
+    {
+        id: 12,
+        url_imagem: "/images/slide3.png",
+        descricao: "Banner 12",
+        ativo: false
+    },
+
+]
