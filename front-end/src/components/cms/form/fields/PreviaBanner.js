850bf338fcd2ee85e32d91ad94b1e7461f64a593
Amanda Oliveira <amandaol7b@gmail.com>
2025-09-04 08:28:55 -0300

    fix: ajustes na previa

--- front-end/src/components/cms/form/fields/PreviaBanner.js
+++ front-end/src/components/cms/form/fields/PreviaBanner.js
@@ -4,11 +4,8 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 export default function PreviaBanner({ fileList }) { 
   return (
     <div className=" w-full sm:min-h-[200px] min-h-[500px] flex items-center justify-center border border-gray-300 rounded-2xl bg-white relative">
-      <p className="absolute top-[-18px] left-10 font-bold text-lg">Prévia</p>
+      <p className="absolute top-[-18px] left-10 font-bold text-lg bg-white">Prévia</p>
       <div className="flex justify-between w-full">
-        <button className="custom-prev absolute top-1/2 left-0 -translate-y-1/2 z-20">
-          <IoIosArrowBack size={70} color="#374a8c" />
-        </button>
         <div className="lg:w-15 sm:w-3 h-80 w-5 bg-gray-200 rounded-r-3xl" />
         {fileList.length > 0 ? (
           <div className="lg:w-100 sm:w-75 w-100 h-80 bg-gray-200 rounded-3xl">
@@ -24,9 +21,6 @@ export default function PreviaBanner({ fileList }) {
           <div className="lg:w-100 sm:w-75 h-80 w-100 bg-gray-200 rounded-3xl" />
         )}
         <div className="lg:w-15 sm:w-3 h-80 w-5 bg-gray-200 rounded-l-3xl" />
-        <button className="custom-next absolute top-1/2 right-0 -translate-y-1/2 z-20">
-          <IoIosArrowForward size={70} color="#374a8c" />
-        </button>
       </div>
     </div>
   );
