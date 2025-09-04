cbca27b64f9391df239d9b80771fa5912676fb77
Giovana Albanês <albanesgiovana@gmail.com>
2025-09-04 15:40:09 -0300

    feat: implementa a rota POST de recomendacao_imovel

---
+++ back-end/src/controllers/recomendacaoImovelController.js
@@ -0,0 +1,18 @@
+// controllers/recomendacaoImovelController.js
+import connection from '../config/sequelize-config.js';
+
+export const createRecomendacaoImovel = async (req, res) => {
+  const { usuario_id, imovel_id, data_visita } = req.body;
+
+  try {
+    const result = await connection.query(
+      'INSERT INTO recomendacao_imovel (usuario_id, imovel_id, data_visita) VALUES (?, ?, ?)',
+      { bind: [usuario_id, imovel_id, data_visita] }
+    );
+    res.status(201).json('Novo registro na tabela recomendacao_imovel.'); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro interno do servidor.' });
+  }
+};
+
