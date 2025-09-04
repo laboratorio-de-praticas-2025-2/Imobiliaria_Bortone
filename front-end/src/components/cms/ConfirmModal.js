a5cd601f515757d917b57e25fc90158f4c7db1a0
Amanda Oliveira <amandaol7b@gmail.com>
2025-09-03 22:19:17 -0300

    feat: adiciona icone

--- front-end/src/components/cms/ConfirmModal.js
+++ front-end/src/components/cms/ConfirmModal.js
@@ -1,10 +1,11 @@
-export default function ConfirmModal({ message, onConfirm, onCancel}){
-    return(
-        <div className="fixed inset-0 bg-black/30 flex items-center justify-center z-50">
-            <div className="bg-white p-6 rounded-lg shadow-lg w-96">
-                <p className="mb-4">{message}</p>
-            </div>
-        </div>
-
-    )
+import { PiWarningCircle } from "react-icons/pi";
+export default function ConfirmModal({ message, onConfirm, onCancel }) {
+  return (
+    <div className="fixed inset-0 bg-black/30 flex items-center justify-center z-50">
+      <div className="bg-white p-10 rounded-2xl w-96">
+        <PiWarningCircle className="text-[var(--secondary)]" size={60} />
+        <p className="mb-4">{message}</p>
+      </div>
+    </div>
+  );
 }
