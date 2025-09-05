53b08cfcd30f5c4eb7bcdcc66352834e51575da4
Amanda Oliveira <amandaol7b@gmail.com>
2025-09-05 13:56:20 -0300

    feat: ajusta nome da prop

--- front-end/src/app/admin/cms-banner/page.js
+++ front-end/src/app/admin/cms-banner/page.js
@@ -50,7 +50,7 @@ export default function CmsBannerPage() {
               {paginatedBanners.length > 0 ? (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 justify-center">
                   {paginatedBanners.map((banner) => (
-                    <Card key={banner.id} banner={banner} />
+                    <Card key={banner.id} item={banner} />
                   ))}
                 </div>
               ) : (
