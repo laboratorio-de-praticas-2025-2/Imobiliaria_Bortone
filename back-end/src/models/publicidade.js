4bf6cf8ee6de82fea360bcf3b62af3f80f7ed696
MauricioRVeiga <mauricio.rgv15@gmail.com>
2025-09-08 17:27:27 -0300

    feat(model): atualiza model Publicidade para usar sequelize-config.js e remover associate

--- back-end/src/models/publicidade.js
+++ back-end/src/models/publicidade.js
@@ -1,46 +1,39 @@
-export default (sequelize, DataTypes) => {
-  const Publicidade = sequelize.define(
-    "Publicidade",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      titulo: {
-        type: DataTypes.STRING(100),
-        allowNull: false, // obrigatório para criar
-      },
-      conteudo: {
-        type: DataTypes.TEXT,
-        allowNull: false, // obrigatório para criar
-      },
-      url_imagem: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-      },
-      usuario_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: "usuario", // nome exato da tabela no banco
-          key: "id",
-        },
+import { DataTypes } from "sequelize";
+import sequelize from "../config/sequelize-config.js";
+
+const Publicidade = sequelize.define(
+  "Publicidade",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    titulo: {
+      type: DataTypes.STRING(100),
+      allowNull: false, // obrigatório para criar
+    },
+    conteudo: {
+      type: DataTypes.TEXT,
+      allowNull: false, // obrigatório para criar
+    },
+    url_imagem: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    usuario_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      references: {
+        model: "usuario", //nome exato da tabela no banco
+        key: "id",
       },
     },
-    {
-      tableName: "publicidade",
-      timestamps: false, // se quiser createdAt/updatedAt, mude para true
-    }
-  );
+  },
+  {
+    tableName: "publicidade",
+    timestamps: false,
+  }
+);
 
-  // Relacionamento: cada publicidade pertence a um usuário
-  Publicidade.associate = (models) => {
-    Publicidade.belongsTo(models.Usuario, {
-      foreignKey: "usuario_id",
-      as: "usuario",
-    });
-  };
-
-  return Publicidade;
-};
+export default Publicidade;
