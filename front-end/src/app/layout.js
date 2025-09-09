ec8069538cf3cbca7337d10b2b8040d344c096d8
Amanda Oliveira <amandaol7b@gmail.com>
2025-09-09 17:35:48 -0300

    refactor: remove Google Maps autocomplete

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -98,11 +98,6 @@ export default function RootLayout({ children }) {
             "logo": "https://imobiliaria-bortone.vercel.app/favicon.ico"
           }
         `}</Script>
-
-        <Script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
-          strategy="beforeInteractive"
-        />
       </head>
       <body className="antialiased">
         {children}
