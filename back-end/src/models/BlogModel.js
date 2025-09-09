d4000b049f264b9bf5f2f1743a4d684da989cc91
LuizWKM <luizricardowm@outlook.com>
2025-09-09 09:10:11 -0300

    feat(backend): adicionado model de Blog.

---
+++ back-end/src/models/BlogModel.js
@@ -0,0 +1,40 @@
+// Esse é apenas um arquivo para manter o diretório ativo
+import { Sequelize } from "sequelize";
+import connection from '../config/sequelize-config.js'
+
+import { Sequelize } from "sequelize";
+import connection from '../config/sequelize-config.js'
+
+const Blog = connection.define("blog", {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    titulo: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+    conteudo: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+    data_publicacao: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+    },
+    url_imagem: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+    usuario_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    }},
+    {
+    tableName: 'blog',
+    timestamps: false, 
+});
+
+export default Blog;
