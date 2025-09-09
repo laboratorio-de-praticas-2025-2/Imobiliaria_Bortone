cfd7e7df8692698a20cbc5ab5725b4237f1e3b19
LuizWKM <luizricardowm@outlook.com>
2025-09-09 09:41:13 -0300

    feat(backend): adicionado TerrenoModel.

---
+++ back-end/src/models/TerrenoModel.js
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize";
+import connection from '../config/sequelize-config.js'
+
+const Terreno = connection.define("terreno", {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    imovel_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    tipo_terreno: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    }},
+    {
+    tableName: 'terreno',
+    timestamps: false, 
+});
+
+export default Terreno;
