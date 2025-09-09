5f8b92098a89b1f7d1ec0261cab01fa7bf2f371f
LuizWKM <luizricardowm@outlook.com>
2025-09-09 09:58:44 -0300

    feat(backend): adiciona arquivo central de associações Associations.js, para associar as fk's corretamente.

---
+++ back-end/src/models/Associations.js
@@ -0,0 +1,21 @@
+import Imovel from './ImovelModel.js';
+import Casa from './CasaModel.js';
+import Terreno from './TerrenoModel.js';
+import Blog from './BlogModel.js';
+import Usuario from './UsuarioModel.js';
+
+// 🔹 Imovel ↔ Casa
+Imovel.hasOne(Casa, { foreignKey: 'imovel_id' });
+Casa.belongsTo(Imovel, { foreignKey: 'imovel_id' });
+
+// 🔹 Imovel ↔ Terreno
+Imovel.hasOne(Terreno, { foreignKey: 'imovel_id' });
+Terreno.belongsTo(Imovel, { foreignKey: 'imovel_id' });
+
+// 🔹 Usuario ↔ Imovel
+Usuario.hasMany(Imovel, { foreignKey: 'usuario_id' });
+Imovel.belongsTo(Usuario, { foreignKey: 'usuario_id' });
+
+// 🔹 Usuario ↔ Blog
+Usuario.hasMany(Blog, { foreignKey: 'usuario_id' });
+Blog.belongsTo(Usuario, { foreignKey: 'usuario_id' });
