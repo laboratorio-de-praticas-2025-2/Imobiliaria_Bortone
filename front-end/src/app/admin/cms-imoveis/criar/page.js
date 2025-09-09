cca4afcddb9cc9492d6bc91a45593c92221a5b5a
Igor Leite Gomes <igorgomeleite@gmail.com>
2025-09-09 19:57:03 -0300

    feat: adiciona DropdownField e atualizar o formulário de criação de propriedade
    Introduzimos um componente DropdownField reutilizável para menus suspensos de formulários. Refatoramos a página de criação de propriedades para usar DropdownField para "Tipo" e "Status", adicionamos campos de descrição e upload de imagens e atualizamos o layout do formulário para melhor usabilidade.

--- front-end/src/app/admin/cms-imoveis/criar/page.js
+++ front-end/src/app/admin/cms-imoveis/criar/page.js
@@ -1,13 +1,17 @@
 "use client";
 import Form from "@/components/cms/form";
+import { useState } from "react";
 import RadioField from "@/components/cms/form/fields/RadioField";
 import TextField from "@/components/cms/form/fields/TextField";
 import FormButton from "@/components/cms/form/fields/Button";
 import { MdPersonAdd } from "react-icons/md";
 import Sidebar from "@/components/cms/Sidebar";
+import TextAreaField from "@/components/cms/form/fields/TextAreaField";
+import DropdownField from "@/components/cms/form/fields/Dropdown";
+import { Form as FormAntd } from "antd";
+import UploadField from "@/components/cms/form/fields/UploadField";
 
-export default function CriarUserPage() {
-
+export default function CriarImovelPage() {
   const onFinish = (values) => {
     console.log("Success:", values);
   };
@@ -15,54 +19,122 @@ export default function CriarUserPage() {
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
+  const [tipoSelecionado, setTipoSelecionado] = useState("Selecione o Tipo");
+  const [statusSelecionado, setstatusSelecionado] =
+    useState("Selecione o status");
+  const [fileList, setFileList] = useState([]);
+
+  const options = ["Casa", "Terreno"];
+  const status = ["Disponivel", "Indisponivel", "Vendido", "Alugado"];
 
   return (
     <>
       <Sidebar />
       <div className="md:ml-20">
-        <Form.Body title="Usuários | Cadastro">
-          <Form.FormHeader href="/admin/cms-usuarios" />
+        <Form.Body title="Imóveis | Cadastro">
+          <Form.FormHeader href="/admin/cms-imoveis" />
           <Form.FormBody onFinish={onFinish} onFinishFailed={onFinishFailed}>
             <div className="flex flex-col sm:flex-row w-full">
               {/* Coluna do Formulário */}
-              <div className="sm:w-[50%] flex flex-col gap-6 items-end">
-                <TextField
-                  name="nome"
-                  label="Nome"
-                  placeholder="Nome do Usuário"
-                  className="!w-[100%]"
-                />
+              <div className="sm:w-[50%] flex flex-col gap-6 items-start">
+                <div className=" flex flex-row gap-6">
+                  <FormAntd.Item
+                    label={"Tipo"}
+                    rules={[
+                      { required: true, message: "Este campo é obrigatório!" },
+                    ]}
+                    className={`custom-form-item `}
+                    labelCol={{ span: 24 }}
+                  >
+                    <DropdownField
+                      placeholder="Selecione o Tipo"
+                      label="Tipo"
+                      options={options}
+                      selected={tipoSelecionado}
+                      setSelected={setTipoSelecionado}
+                      handleSelect={(option) => setTipoSelecionado(option)}
+                      width={"w-full"}
+                      classname="bg-white hover:bg-[#EEF0F9] w-full "
+                    />
+                  </FormAntd.Item>
 
-                <TextField
-                  name="email"
-                  label="Email"
-                  placeholder="Email do Usuário"
-                  className="!w-[100%]"
+                  <FormAntd.Item
+                    label={"Status"}
+                    rules={[
+                      { required: true, message: "Este campo é obrigatório!" },
+                    ]}
+                    className={`custom-form-item `}
+                    labelCol={{ span: 24 }}
+                  >
+                    <DropdownField
+                      placeholder="Selecione o Tipo"
+                      options={status}
+                      selected={statusSelecionado}
+                      setSelected={setstatusSelecionado}
+                      handleSelect={(status) => setstatusSelecionado(status)}
+                      width={"w-full"}
+                      classname="bg-white hover:bg-[#EEF0F9] w-full "
+                    />
+                  </FormAntd.Item>
+                </div>
+                <UploadField
+                  name="imagem"
+                  label="Imagens"
+                  multiple={false}
+                  className="!w-fit"
+                  fileList={fileList}
+                  setFileList={setFileList}
                 />
+                <TextAreaField
+                  name="descricao"
+                  label="Descrição"
+                  placeholder="Corpo da descrição"
+                  rows={12}
+                  className="!w-full !h-full"
+                />
+                <FormButton text="Cadastrar" icon={<MdPersonAdd />} />
+              </div>
+              <div className="sm:w-[50%] flex flex-col gap-6 items-start">
+                <div className=" flex flex-row gap-6">
+                  <FormAntd.Item
+                    label={"Tipo"}
+                    rules={[
+                      { required: true, message: "Este campo é obrigatório!" },
+                    ]}
+                    className={`custom-form-item `}
+                    labelCol={{ span: 24 }}
+                  >
+                    <DropdownField
+                      placeholder="Selecione o Tipo"
+                      label="Tipo"
+                      options={options}
+                      selected={tipoSelecionado}
+                      setSelected={setTipoSelecionado}
+                      handleSelect={(option) => setTipoSelecionado(option)}
+                      width={"w-full"}
+                      classname="bg-white hover:bg-[#EEF0F9] w-full "
+                    />
+                  </FormAntd.Item>
 
-                <RadioField
-                  name="nivel"
-                  label="Nível"
-                  options={[
-                    { label: "Administrador", value: "administrador" },
-                    { label: "Usuário Padrão", value: "usuario" },
-                  ]}
-                  className="!w-[100%]"
-                />
-
-                <TextField
-                  name="celular"
-                  label="Celular"
-                  placeholder="Celular do Usuário"
-                  className="!w-[100%]"
-                />
-
-                <TextField
-                  name="senha"
-                  label="Senha"
-                  placeholder="Senha do Usuário"
-                  className="!w-[100%]"
-                />
+                  <FormAntd.Item
+                    label={"Status"}
+                    rules={[
+                      { required: true, message: "Este campo é obrigatório!" },
+                    ]}
+                    className={`custom-form-item `}
+                    labelCol={{ span: 24 }}
+                  >
+                    <DropdownField
+                      placeholder="Selecione o Tipo"
+                      options={status}
+                      selected={statusSelecionado}
+                      setSelected={setstatusSelecionado}
+                      handleSelect={(status) => setstatusSelecionado(status)}
+                      width={"w-full"}
+                      classname="bg-white hover:bg-[#EEF0F9] w-full "
+                    />
+                  </FormAntd.Item>
+                </div>
                 <FormButton text="Cadastrar" icon={<MdPersonAdd />} />
               </div>
             </div>
---
+++ front-end/src/components/cms/form/fields/Dropdown.js
@@ -0,0 +1,67 @@
+"use client";
+import useHandleClickOutside from "@/hooks/useHandleClickOutside";
+import { useRef, useState } from "react";
+import { FaAngleDown } from "react-icons/fa6";
+import { HiX } from "react-icons/hi";
+
+export default function DropdownField({
+  options,
+  classname,
+  selected,
+  placeholder,
+  handleSelect,
+  setSelected,
+  width,
+}) {
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useHandleClickOutside(dropdownRef, () => setOpen(false));
+
+  const handleClear = (e) => {
+    e.stopPropagation();
+    setSelected(placeholder);
+    setOpen(false);
+  };
+
+  return (
+
+      <div
+        ref={dropdownRef}
+        className={`relative inline-block text-left ${width || "w-[80%]"}`}
+      >
+        <button
+          onClick={() => setOpen(!open)}
+          className={`flex items-center w-fit custom-input justify-center !rounded-full bg-[#EEF0F9] px-8 py-2 !text-[var(--primary)] font-medium focus:outline-none cursor-pointer hover:bg-[#E0E3F1] transition-colors ${classname}`}
+        >
+          <p className={`font-bold text-lg !mr-2`}>{selected}</p>
+          {setSelected && selected !== placeholder ? (
+            <HiX
+              className="h-4 w-4 text-[var(--primary)] cursor-pointer"
+              onClick={handleClear}
+            />
+          ) : (
+            <FaAngleDown className="h-4 w-4 text-[var(--primary)]" />
+          )}
+        </button>
+        {/* Menu */}
+        {open && (
+          <div className="absolute mt-2 min-w-full right-0 rounded-2xl bg-white shadow-lg ring-1 ring-black/5 z-10">
+            <div className="flex flex-col gap-0.5">
+              {options.map((option) => (
+                <button
+                  key={option}
+                  onClick={() => handleSelect(option)}
+                  className={`px-4 py-2 !text-[var(--primary)] rounded-2xl hover:bg-[#EEF0F9] text-left ${
+                    option === selected ? "bg-[#EEF0F9]" : ""
+                  }`}
+                >
+                  {option}
+                </button>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+  );
+}
