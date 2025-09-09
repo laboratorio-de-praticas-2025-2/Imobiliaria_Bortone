8d724c14bbc85067338931940ffe7d2976f8764a
Daniel Augusto Mandira <105872910+DanielMandira@users.noreply.github.com>
2025-09-09 15:13:36 -0300

    Revert "feat: Envio de e-mail e construção da API (#156)"
    This reverts commit 8d89377b9b6ec6750bde5ffbef246e37a292f4c8.

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,44 +1,44 @@
-import 'dotenv/config';
 import express from "express";
+import router from './routes/recomendacaoImovelRoutes.js';
 import cors from "cors";
-
 import connection from "./config/sequelize-config.js";
-
-// Rotas
-import agendamentoRouter from './routes/agendamentoRoute.js';
-import recomendacaoRouter from './routes/recomendacaoImovelRoutes.js';
-import healthRouter from "./routes/route.js";
+// Exemplo de como importar rotas
+ import healthRouter from "./routes/route.js"; 
 
 const app = express();
 
 // Middlewares
 app.use(cors()); // Habilita o CORS para todas as origens
-app.use(express.json());
+app.use(express.json()); // Para parsear JSON
 app.use(express.urlencoded({ extended: false }));
 
 // Rotas
-app.get('/', (req, res) => res.send('API rodando 🚀'));
-app.use('/agendamentos', agendamentoRouter);
-app.use('/recomendacoes', recomendacaoRouter);
-app.use('/health', healthRouter);
+// Exemplo de como usar as rotas
+app.use("/", router, healthRouter);
 
-// Banco de dados
 connection
   .authenticate()
   .then(() => {
     console.log("Conexão com banco de dados realizada com sucesso!");
   })
   .catch((error) => {
-    console.log("Erro ao conectar com banco de dados:", error);
+    console.log(error);
   });
 
-// Porta
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, function (erro) {
   if (erro) {
     console.log("Ocorreu um erro! Erro: ", erro);
   } else {
-    console.log(`Servidor iniciado com sucesso na porta ${PORT}! 🚀`);
+    console.log(`Servidor iniciado com sucesso na porta ${PORT}!`);
   }
 });
+
+// // Exemplo para produção
+// Para um ambiente de produção, é uma boa prática restringir as origens permitidas, como no exemplo abaixo:
+// const corsOptions = {
+//   origin: 'https://imobiliaria-bortone.vercel.app' // Substitua pelo domínio do seu frontend
+// };
+
+// app.use(cors(corsOptions));
