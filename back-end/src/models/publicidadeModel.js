e530278333848e439667a3dd002f78714b653bf4
DSM3 <fatec-dsm3@academico.intra>
2025-09-08 21:57:03 -0300

    feat(model): adiciona campo 'ativo' ao model Publicidade

---
+++ back-end/src/models/publicidadeModel.js
@@ -0,0 +1,45 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/sequelize-config.js";
+
+const PublicidadeModel = sequelize.define(
+  "Publicidade",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    titulo: {
+      type: DataTypes.STRING(100),
+      allowNull: false, // obrigatório para criar
+    },
+    conteudo: {
+      type: DataTypes.TEXT,
+      allowNull: false, // obrigatório para criar
+    },
+    url_imagem: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    usuario_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "usuario", //nome exato da tabela no banco
+        key: "id",
+      },
+    },
+    ativo: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  },
+  {
+    tableName: "publicidade",
+    timestamps: false,
+  }
+);
+
+
+export default PublicidadeModel;
