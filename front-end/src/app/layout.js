e9a265291ed509e790e69fc07f78782cdaecede3
Joao1paulo <joaopaulo.rustichelli@gmail.com>
2025-09-10 14:05:26 -0300

    fix: Remove o chatButton das telas de Bem-vindo, Login e Cadastro

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -1,4 +1,3 @@
-// app/layout.js
 import "@/styles/globals.css";
 import "@/styles/login.css";
 import "@/styles/map.css";
@@ -8,13 +7,11 @@ import "@/styles/imoveis.css";
 import "@/styles/blog.css";
 import "@/styles/faq.css";
 import "antd/dist/reset.css";
-import ChatLauncherClient from "@/components/chat/chatLauncherClient";
 import "@/styles/cms.css";
 import Script from "next/script";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-
-
+import ChatWrapper from "@/components/chat/chatWrapper";
 
 
 export default function RootLayout({ children }) {
@@ -106,11 +103,10 @@ export default function RootLayout({ children }) {
       </head>
       <body className="antialiased">
         {children}
-        {/* Client wrapper que controla abrir/fechar */}
-        <ChatLauncherClient />
         {/* Analytics Vercel */}
         <Analytics />
         <SpeedInsights />
+        <ChatWrapper />
       </body>
     </html>
   );
