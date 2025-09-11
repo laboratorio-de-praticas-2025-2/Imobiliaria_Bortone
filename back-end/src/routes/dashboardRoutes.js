59c042bdf556c63a19c1d162bd2d386213a6bb58
arthurfudali <arthur.fudali@gmail.com>
2025-09-10 21:49:47 -0300

    refactor(routes): muda o nome para dashboardRouterrefactor(routes): muda o nome para dashboardRouter
    Muda a variável router para 'dashboardRouter' para melhor clareza e muda os exports de acordo.

--- back-end/src/routes/dashboardRoutes.js
+++ back-end/src/routes/dashboardRoutes.js
@@ -1,8 +1,8 @@
 import dashboardController from "../controllers/dashboardController.js";
 import express from "express";
 
-const router = express.Router();
+const dashboardRouter = express.Router();
 
 router.get("/dashboard", dashboardController.findInfosDashboard);
 
-export default router;
+export default dashboardRouter;
