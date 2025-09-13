761d4efdbefe1d532b4cb6be1f757ebafaddce58
TheLimaLeo <leolima1999pro@gmail.com>
2025-09-13 14:29:55 -0300

    feat: Adiciona melhorias no blog controller

--- back-end/src/controllers/blogController.js
+++ back-end/src/controllers/blogController.js
@@ -1,90 +1,100 @@
 import blogService from "../services/blogService.js";
+import { isValidObjectId } from "mongoose";
 
-// Colocar o controller em uma classe
-
-export const createArtigo = async (req, res) => {
-  try {
-    const novoArtigo = await blogService.createArtigo(req.body);
-    res.status(201).json({
-      messsage: "Novo artigo criado com sucesso.",
-      data: novoArtigo,
-    });
-  } catch (err) {
-    console.error("Erro ao criar artigo:", err.message);
-    res.status(500).json({ error: "Erro ao criar o artigo." });
-  }
-};
-
-export const getArtigoById = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const artigo = await blogService.getArtigoById(id);
-    if (!artigo) {
-      return res
-        .status(404)
-        .json({ error: `Artigo com o ID ${id} não encontrado.` });
+class BlogController {
+  
+  async createArtigo(req, res) {
+    try {
+      const novoArtigo = await blogService.createArtigo(req.body);
+      res.status(201).json({
+        message: "Novo artigo criado com sucesso.",
+        data: novoArtigo,
+      });
+    } catch (err) {
+      console.error("Erro ao criar artigo:", err.message);
+      res.status(500).json({ error: "Erro ao criar o artigo." });
     }
-    res.status(200).json({
-      message: "Artigo obtido com sucesso.",
-      data: artigo,
-    });
-  } catch (err) {
-    console.error(`Erro ao buscar o artigo de ID ${id}:`, err.message);
-    res.status(500).json({ error: `Erro ao buscar o artigo de ID ${id}.` });
   }
-};
 
-// essa rota precisa passar os parametros de pesquisa ou ordenação para o service
-export const getAllArtigos = async (req, res) => {
-  try {
-    const artigos = await blogService.getAllArtigos();
-    res.status(200).json({ artigo: artigos });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Erro interno do servidor." });
-  }
-};
+  async getArtigoById(req, res) {
+    const { id } = req.params;
+    try {
+      if (!isValidObjectId(id)) {
+        return res.status(400).json({ error: "ID de artigo inválido." });
+      }
 
-const updateArtigo = async (req, res) => {
-  try {
-    if (objectId.isValid(req.params.id)) {
-      const id = req.params.id;
-      const { usuario_id, titulo, conteudo, data_publicacao, url_imagem } =
-        req.body;
-      await blogService.updateArtigo(
-        usuario_id,
-        titulo,
-        conteudo,
-        data_publicacao,
-        url_imagem
-      );
-      res.sendStatus(200);
-    } else {
-      res.sendStatus(400);
+      const artigo = await blogService.getArtigoById(id);
+      if (!artigo) {
+        return res
+          .status(404)
+          .json({ error: `Artigo com o ID ${id} não encontrado.` });
+      }
+      res.status(200).json({
+        message: "Artigo obtido com sucesso.",
+        data: artigo,
+      });
+    } catch (err) {
+      console.error(`Erro ao buscar o artigo de ID ${id}:`, err.message);
+      res.status(500).json({ error: `Erro ao buscar o artigo de ID ${id}.` });
     }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Erro interno do servidor." });
   }
-};
 
-export const deleteArtigo = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const artigoDeletado = await blogService.deleteArtigo(id);
-    if (!artigoDeletado) {
-      return res
-        .status(404)
-        .json({ error: `Artigo com o ID ${id} não encontrado para exclusão.` });
+  async getAllArtigos(req, res) {
+    try {
+      const query = req.query;
+      const artigos = await blogService.getAllArtigos(query); // Passa os parâmetros para o service
+      res.status(200).json({ artigos: artigos });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Erro interno do servidor." });
     }
-    res.status(200).json({
-      message: "Artigo deletado com sucesso.",
-      data: artigoDeletado,
-    });
-  } catch (err) {
-    console.error(`Erro ao deletar o artigo de ID ${id}:`, err.message);
-    res.status(500).json({ error: `Erro ao deletar o artigo de ID ${id}.` });
   }
-};
 
-// Exporta o classe
+  async updateArtigo(req, res) {
+    const { id } = req.params;
+    try {
+      if (!isValidObjectId(id)) {
+        return res.status(400).json({ error: "ID de artigo inválido." });
+      }
+
+      const artigoAtualizado = await blogService.updateArtigo(id, req.body);
+      if (!artigoAtualizado) {
+        return res
+          .status(404)
+          .json({ error: `Artigo com o ID ${id} não encontrado para atualização.` });
+      }
+
+      res.status(200).json({
+        message: "Artigo atualizado com sucesso.",
+        data: artigoAtualizado,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Erro interno do servidor." });
+    }
+  }
+
+  async deleteArtigo(req, res) {
+    const { id } = req.params;
+    try {
+      if (!isValidObjectId(id)) {
+        return res.status(400).json({ error: "ID de artigo inválido." });
+      }
+      const artigoDeletado = await blogService.deleteArtigo(id);
+      if (!artigoDeletado) {
+        return res
+          .status(404)
+          .json({ error: `Artigo com o ID ${id} não encontrado para exclusão.` });
+      }
+      res.status(200).json({
+        message: "Artigo deletado com sucesso.",
+        data: artigoDeletado,
+      });
+    } catch (err) {
+      console.error(`Erro ao deletar o artigo de ID ${id}:`, err.message);
+      res.status(500).json({ error: `Erro ao deletar o artigo de ID ${id}.` });
+    }
+  }
+}
+
+export default new BlogController();
