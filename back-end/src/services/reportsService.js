bf8d9b6ac1c95a0e1a965012921307a59c9a2017
SouzaDiegoCl <souzadiegocl@gmail.com>
2025-09-16 04:20:31 -0300

    feat (reports-pdf): Adiciona pdf de report geral com dados dinâmicos da query

--- back-end/src/services/reportsService.js
+++ back-end/src/services/reportsService.js
@@ -1,57 +1,62 @@
 import Imovel from "../models/Imovel.js";
 import puppeteer from "puppeteer"; //IMPORTA PUPPETEER PARA CRIACAO DO PDF
+import sequelize from "../config/sequelize-config.js";
+import imagemParaBase64 from "../utils/converterImagemParaBase64.js";
+import fs from "fs";
+import handlebars from "handlebars";
 
 class ReportService {
-  
-  static async buscarDados(filtros={}) {
-    return await Imovel.findAll({ where: filtros });
+  static async buscarDados(filtros = {}) {
+    const query = fs.readFileSync("./src/queries/dados-pdf-geral.sql", "utf-8");
+
+    const resultados = await sequelize.query(query, {
+      type: sequelize.QueryTypes.SELECT,
+      replacements: filtros,
+      logging: false,
+    });    
+
+    return resultados;
   }
 
-  // ESTRUTURA O HTML
-  static async gerarHTML(dados) {
-    const rows = dados.map(i => `
-      <tr>
-        <td>${i.titulo}</td>
-        <td>R$ ${i.preco.toFixed(2)}</td>
-        <td>${i.vendido ? "Sim" : "Não"}</td>
-      </tr>
-    `).join("");
-
-    return `
-      <html>
-        <head>
-          <style>
-            body { font-family: Arial, sans-serif; padding: 20px; }
-            h1 { color: #2E86C1; text-align: center; }
-            table { width: 100%; border-collapse: collapse; margin-top: 20px; }
-            th, td { border: 1px solid #ddd; padding: 8px; text-align: center; }
-            th { background: #f4f4f4; }
-          </style>
-        </head>
-        <body>
-          <h1>Relatório de Imóveis</h1>
-          <table>
-            <tr><th>Título</th><th>Preço</th><th>Vendido</th></tr>
-            ${rows}
-          </table>
-        </body>
-      </html>
-    `;
-  }
-
-  // GERA PDF 
+  // GERA PDF
   static async gerarPDF(filtros = {}) {
-    const dados = await this.buscarDados(filtros);
-    const html = await this.gerarHTML(dados);
+    handlebars.registerHelper("json", function (context) {
+      return JSON.stringify(context);
+    });
+    //Informações extra para geração do PDF    
+    const formattedDate = new Date().toLocaleDateString("pt-BR", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
 
-    const browser = await puppeteer.launch();
+    const dados = await this.buscarDados(filtros);
+    const resultado = JSON.parse(dados[0].resultado);
+
+    const templateHtml = fs.readFileSync(
+      "./src/templates/report-template-pdf.html",
+      "utf8"
+    );
+
+    const template = handlebars.compile(templateHtml);
+
+    // Preenche template com dados
+    const html = template({
+      dados: dados,   
+      data_emissao: formattedDate,
+    });
+
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+      headless: "new",
+    });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "networkidle0" });
 
     const pdf = await page.pdf({
       format: "A4",
       printBackground: true,
-      margin: { top: "20px", right: "20px", bottom: "20px", left: "20px" }
+      margin: { top: "0", right: "0", bottom: "0", left: "0" },
     });
 
     await browser.close();
