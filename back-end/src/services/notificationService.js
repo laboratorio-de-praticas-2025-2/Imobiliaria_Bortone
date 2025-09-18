e47cab03b09a5c69ea9dd1746c34ab98418404e5
Gabrielll04 <gabrielggcarneiro04@gmail.com>
2025-09-18 20:14:03 -0300

    feat: cria a classe NotificationService para envio de notificações

---
+++ back-end/src/services/notificationService.js
@@ -0,0 +1,11 @@
+class NotificationService {
+
+  // Função com a finalidade é acionar o SocketManager para o envio das notificações.
+  async dispararAlertaNovoImovel(novoImovel) {
+    const { latitude, longitude, preco, area, tipo_negociacao, status } = novoImovel;
+
+    return
+  }
+}
+
+export default NotificationService;
