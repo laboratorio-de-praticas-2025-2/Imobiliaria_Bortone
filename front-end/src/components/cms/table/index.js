18dfaff5adc8258aba95585e5c82a2387f7d828d
Vinicius Leal Alves <avieirakunzen@gmail.com>
2025-09-26 13:59:49 -0300

    remove chamadas de arquivos excluidos

--- front-end/src/components/cms/table/index.js
+++ front-end/src/components/cms/table/index.js
@@ -3,19 +3,15 @@ import Table from "./Table";
 import TableBody from "./TableBody";
 import TableFooter from "./TableFooter";
 import TableHeader from "./TableHeader";
-import TableFooterPublicidade from "./TableFooterPublicidade";
-import TableHeaderPublicidade from "./TableHeaderPublicidade";
 
-export { Body, Table, TableBody, TableFooter, TableHeader, TableFooterPublicidade, TableHeaderPublicidade };
+export { Body, Table, TableBody, TableFooter, TableHeader};
 
 const CMS = {
   Body,
   Table,
   TableHeader,
   TableBody,
-  TableFooter,
-  TableFooterPublicidade,
-  TableHeaderPublicidade,
+  TableFooter
 };
 
 export default CMS;
