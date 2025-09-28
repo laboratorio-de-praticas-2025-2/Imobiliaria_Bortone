745a166ac87d5f3c0ba72862f12c568512a44946
Bruno Freitas <brunofreitasd11@gmail.com>
2025-09-28 14:01:59 -0300

    Feat: Integração da vitrine trazendo imagens

--- front-end/src/components/vitrine/ImovelCard.js
+++ front-end/src/components/vitrine/ImovelCard.js
@@ -2,17 +2,17 @@ import ImageCarroussel from "./ImageCarroussel";
 
 export default function ImovelCard({imovel}) {
     return (
-      <div className="hover:sm:shadow-lg sm:rounded-xl transition-shadow cursor-pointer gap-4 sm:w-fit flex flex-col sm:p-4">
-        <ImageCarroussel imovel={imovel} />
+      <div className="hover:sm:shadow-lg sm:rounded-xl transition-shadow cursor-pointer gap-4 flex flex-col sm:p-4 w-full sm:w-72 md:w-80">
+         <ImageCarroussel imovel={imovel} />
         <p className="md:text-xl text-2xl font-bold text-[var(--primary)] px-4 sm:px-0">
           R$ {imovel.preco.toLocaleString()}
         </p>
         <div className="lg:text-lg sm:text-base text-lg sm:font-normal font-bold text-[var(--primary)] px-4 sm:px-0">
           <p className="flex flex-wrap items-center gap-1">
             {imovel.area} m²{" "}
-            {imovel.tipo === "Casa" && (
+            {(imovel.tipo === "Casa" || imovel.tipo === "Apartamento") && imovel.casa && (
               <>
-                • {imovel.quartos} Quartos • {imovel.banheiros} Banheiros
+                • {imovel.casa.quartos} Quartos • {imovel.casa.banheiros} Banheiros
               </>
             )}
           </p>
