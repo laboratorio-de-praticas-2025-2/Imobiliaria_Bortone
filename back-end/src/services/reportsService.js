c8614b51a76125aaf8ced4bd0b73440921b52721
SouzaDiegoCl <souzadiegocl@gmail.com>
2025-09-28 08:00:32 -0300

    feat (reports-pdf): Adiciona separacao de views por categoria de report

--- back-end/src/services/reportsService.js
+++ back-end/src/services/reportsService.js
@@ -1,11 +1,29 @@
+import { error } from "console";
 import sequelize from "../config/sequelize-config.js";
 import fs from "fs";
 
 class ReportService {
+  async #executeQuery(query, errorMessage) {
+    try {
+      return await sequelize.query(query, {
+        type: sequelize.QueryTypes.SELECT,
+        logging: true,
+      });
+    } catch (dbError) {
+      console.error(`${errorMessage}:`, {
+        message: dbError.message,
+        query: query.substring(0, 200) + "...",
+        stack: dbError.stack,
+      });
+
+      throw new Error(errorMessage);
+    }
+  }
+
   async buscarDadosParaRelatorio(tipo) {
     try {
       let dadosRelatorio = {};
-      
+
       switch (tipo) {
         case "geral":
           dadosRelatorio = await this.obterDadosGerais();
@@ -39,101 +57,79 @@ class ReportService {
       this.obterDadosImoveis(),
       this.obterDadosUsuarios(),
       this.obterDadosVendas(),
-      this.obterDadosAlugueis()
+      this.obterDadosAlugueis(),
     ]);
 
     return {
       imoveis,
       usuarios,
       vendas,
-      alugueis
+      alugueis,
     };
   }
 
   async obterDadosImoveis() {
-    const totalImoveisQuery = `
-      SELECT COUNT(*) as total FROM imoveis WHERE status = 'disponivel'
-    `;
-    
-    const casasQuery = `
-      SELECT COUNT(*) as total FROM imoveis 
-      WHERE tipo IN ('casa', 'Casa') AND status = 'disponivel'
-    `;
-    
-    const terrenosQuery = `
-      SELECT COUNT(*) as total FROM imoveis 
-      WHERE tipo IN ('terreno', 'Terreno') AND status = 'disponivel'
-    `;
+    const QUERY_IMOVEIS = `select * from dadosRelatorioImoveis`;
+    const ERROR_MESSAGE = "Falha ao recuperar dados de imóveis para relatório";
 
-    const [totalResult, casasResult, terrenosResult] = await Promise.all([
-      sequelize.query(totalImoveisQuery, { type: sequelize.QueryTypes.SELECT }),
-      sequelize.query(casasQuery, { type: sequelize.QueryTypes.SELECT }),
-      sequelize.query(terrenosQuery, { type: sequelize.QueryTypes.SELECT })
-    ]);
-
-    return {
-      totalImoveis: totalResult[0]?.total || 0,
-      totalCasas: casasResult[0]?.total || 0,
-      totalTerrenos: terrenosResult[0]?.total || 0
-    };
+    const queryResponse = await this.#executeQuery(
+      QUERY_IMOVEIS,
+      ERROR_MESSAGE
+    );
+    
+    return JSON.parse(queryResponse[0]?.imoveis || 0);
   }
 
   async obterDadosUsuarios() {
     const totalUsuariosQuery = `
       SELECT COUNT(*) as total FROM usuario WHERE ativo = 1
     `;
-    
+
     const administradoresQuery = `
       SELECT COUNT(*) as total FROM usuario 
       WHERE nivel = 0 AND ativo = 1
     `;
-    
+
     const visitantesQuery = `
       SELECT COUNT(*) as total FROM usuario 
       WHERE nivel = 1 AND ativo = 1
     `;
 
     const [totalResult, adminResult, visitantesResult] = await Promise.all([
-      sequelize.query(totalUsuariosQuery, { type: sequelize.QueryTypes.SELECT }),
-      sequelize.query(administradoresQuery, { type: sequelize.QueryTypes.SELECT }),
-      sequelize.query(visitantesQuery, { type: sequelize.QueryTypes.SELECT })
+      sequelize.query(totalUsuariosQuery, {
+        type: sequelize.QueryTypes.SELECT,
+      }),
+      sequelize.query(administradoresQuery, {
+        type: sequelize.QueryTypes.SELECT,
+      }),
+      sequelize.query(visitantesQuery, { type: sequelize.QueryTypes.SELECT }),
     ]);
 
     return {
       totalUsuarios: totalResult[0]?.total || 0,
       totalAdministradores: adminResult[0]?.total || 0,
-      totalVisitantes: visitantesResult[0]?.total || 0
+      totalVisitantes: visitantesResult[0]?.total || 0,
     };
   }
 
   async obterDadosVendas() {
-    const vendaQuery = `
-      SELECT COUNT(*) as total FROM imoveis 
-      WHERE tipo_negociacao = 'venda' AND status = 'disponivel'
-    `;
+    const QUERY_VENDAS = `select * from dadosRelatorioVendas`;
+    const ERROR_MESSAGE = "Falha ao recuperar dados de vendas para relatório";
 
-    const [vendaResult] = await sequelize.query(vendaQuery, { 
-      type: sequelize.QueryTypes.SELECT 
-    });
-    
-    return {
-      totalVenda: vendaResult?.total || 0
-    };
+    const vendaResult = await this.#executeQuery(QUERY_VENDAS, ERROR_MESSAGE);
+    console.log("aaaaaa");
+    console.log(vendaResult[0]?.vendas);
+
+    return JSON.parse(vendaResult[0]?.vendas || 0);
   }
 
   async obterDadosAlugueis() {
-    const aluguelQuery = `
-      SELECT COUNT(*) as total FROM imoveis 
-      WHERE tipo_negociacao = 'aluguel' AND status = 'disponivel'
-    `;
+    const QUERY_LOCACOES = `select * from dadosRelatorioLocacoes`;
+    const ERROR_MESSAGE = "Falha ao recuperar dados de locações para relatório";
 
-    const [aluguelResult] = await sequelize.query(aluguelQuery, { 
-      type: sequelize.QueryTypes.SELECT 
-    });
-    
-    return {
-      totalLocacao: aluguelResult?.total || 0
-    };
+    const queryResponse = await this.#executeQuery(QUERY_LOCACOES, ERROR_MESSAGE);
+
+    return JSON.parse(queryResponse[0]?.alugueis || 0);
   }
 }
 
