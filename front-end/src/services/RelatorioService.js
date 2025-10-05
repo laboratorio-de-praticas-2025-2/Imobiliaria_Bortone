08bf978c858c7516879eb7ae3bb7182a7c7f0fa5
Joao1paulo <joaopaulo.rustichelli@gmail.com>
2025-10-04 22:22:54 -0300

    feat: adiciona a função de listagem no CMS Relatórios

--- front-end/src/services/RelatorioService.js
+++ front-end/src/services/RelatorioService.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 const url = process.env.NEXT_PUBLIC_API_URL;
 
+// Pega os dados de um relatório específico
 export async function getRelatorioData(tipo) {
   const res = await fetch(`${url}/relatorio?tipo=${tipo}`, {
     method: "GET",
@@ -8,5 +9,16 @@ export async function getRelatorioData(tipo) {
   });
 
   if (!res.ok) throw new Error("Erro ao buscar dados do relatório");
+  return res.json();
+}
+
+// Pega a lista de todos os relatórios
+export async function getAllRelatorios() {
+  const res = await fetch(`${url}/relatorios`, {
+    method: "GET",
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Erro ao buscar lista de relatórios");
   return res.json();
 }
