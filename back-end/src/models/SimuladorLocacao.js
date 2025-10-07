8b84d3ea1e0fc71bc3d5e144d331865cffdd93e0
DSM4 <fatec-dsm4@academico.intra>
2025-10-07 19:57:09 -0300

    feat(model):adiciona model de simulador da aplicação

---
+++ back-end/src/models/SimuladorLocacao.js
@@ -0,0 +1,44 @@
+// src/models/mapaModels.js
+import { DataTypes } from 'sequelize';
+import connection from '../config/sequelize-config.js';
+
+// ---------------------------
+// Model Imovel
+// ---------------------------
+const SimImovel = connection.define('SimuladorImoveis', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  tipo: { type: DataTypes.ENUM('casa', 'apartamento'), allowNull: false },
+  preco: { type: DataTypes.DECIMAL(12,2), allowNull: false }
+}, {
+  tableName: 'SimuladorImoveis',
+  timestamps: false
+});
+
+// ---------------------------
+// Model Financiamento
+// ---------------------------
+const Financiamento = connection.define('financiamento', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  imovel_id: { type: DataTypes.INTEGER, allowNull: false },
+  valorEntrada: { type: DataTypes.DECIMAL(12,2), allowNull: false },
+  valorParcela: { type: DataTypes.DECIMAL(12,2), allowNull: false },
+  quantidadeParcelas: { type: DataTypes.INTEGER, allowNull: false },
+  taxaJuros: { type: DataTypes.DECIMAL(5,2), allowNull: false }
+}, {
+  tableName: 'financiamento',
+  timestamps: false
+});
+
+// ---------------------------
+// Associações
+// ---------------------------
+SimImovel.hasOne(Financiamento, { foreignKey: 'imovel_id' });
+Financiamento.belongsTo(SimImovel, { foreignKey: 'imovel_id' });
+
+// ---------------------------
+// Exportando todos os models
+// ---------------------------
+export {
+  SimImovel,
+  Financiamento,
+};
